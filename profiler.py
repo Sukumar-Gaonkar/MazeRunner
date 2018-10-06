81837a0938f5a5ca6d857552384375666884aeba
Sukumar Gaonkar <sukumargaonkar@gmail.com>
2018-10-06 03:43:21 -0400

    BugFix A* : Considered both visitedNodes and PriorityQueue while adding new element

--- profiler.py
+++ profiler.py
@@ -10,7 +10,7 @@ fileName = "results/profilerResults_" + time.strftime("%Y%m%d-%H%M%S") + ".csv"
 f = open(fileName, "a", 1)
 
 ############## Question 1 ##############
-f.write("Algorithm;Probability;MapSize;IterationNo;ShortestPath;nNodesVisited;MaxFringe;Time;Path;ArenaMap,NodesVisited\n")
+f.write("Algorithm;Probability;MapSize;IterationNo;ShortestPath;nNodesVisited;MaxFringe;Time;Path;ArenaMap\n")
 
 for prob in [x/10 for x in range(1, 10)]:
     for mapSize in range(20, 40):
@@ -21,12 +21,23 @@ for prob in [x/10 for x in range(1, 10)]:
             print()
             shortestPathLength, path, nodesVisited, maxFringeSize, time = algos.AStar(arenaMap, algos.euclideanDistance)
             f.write("{0};{1};{2};{3};{4};{5};{6};{7};{8};{9}\n".format("A*_Euclidean", prob, mapSize, iterNo,
-                                                                   shortestPathLength, len(nodesVisited), maxFringeSize,
+                                                                   shortestPathLength, nodesVisited, maxFringeSize,
                                                                    str(time), path, arenaMap, nodesVisited))
 
             shortestPathLength, path, nNodesVisited, maxFringeSize, time = algos.AStar(arenaMap,
                                                                                        algos.manhattanDistance)
             f.write("{0};{1};{2};{3};{4};{5};{6};{7};{8};{9}\n".format("A*_Manhattan", prob, mapSize, iterNo,
-                                                                   shortestPathLength, len(nodesVisited), maxFringeSize,
+                                                                   shortestPathLength, nodesVisited, maxFringeSize,
                                                                    str(time), path, arenaMap, nodesVisited))
+
+            shortestPathLength, path, nNodesVisited, maxFringeSize, time = algos.DFS(arenaMap)
+            f.write("{0};{1};{2};{3};{4};{5};{6};{7};{8};{9}\n".format("BFS", prob, mapSize, iterNo,
+                                                                       shortestPathLength, nodesVisited, maxFringeSize,
+                                                                       str(time), path, arenaMap, nodesVisited))
+
+            shortestPathLength, path, nNodesVisited, maxFringeSize, time = algos.BFS(arenaMap)
+            f.write("{0};{1};{2};{3};{4};{5};{6};{7};{8};{9}\n".format("DFS", prob, mapSize, iterNo,
+                                                                       shortestPathLength, nodesVisited, maxFringeSize,
+                                                                       str(time), path, arenaMap, nodesVisited))
+
 print("Results stored in : " + fileName)
