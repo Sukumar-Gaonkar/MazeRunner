7600f83b2e7eec1e1ec06d55ed4932fb17fd52d8
bharatoo7 <43590782+bharatoo7@users.noreply.github.com>
2018-10-08 03:47:14 +0530

    Add files via upload

--- resizeeeeee.py
+++ resizeeeeee.py
@@ -2,20 +2,27 @@ import math
 import os
 from PIL import Image
 
-dim = 24
+dim = 50
 k = 600/dim
 print(k)
 j = math.ceil(k)
 size = j, j
-filename = 'mario.gif'
+filename1 = 'mario.gif'
+filename2 = 'wall.gif'
 
-file_parts = os.path.splitext(filename)
-outfile = file_parts[0] + "_" + file_parts[1]
+file_parts1 = os.path.splitext(filename1)
+file_parts2 = os.path.splitext(filename2)
+outfile1 = file_parts1[0] + "_" + file_parts1[1]
+outfile2 = file_parts2[0] + "_" + file_parts2[1]
 
 try:
-    img = Image.open(filename)
+    img = Image.open(filename1)
     img = img.resize(size, Image.ANTIALIAS)
-    img.save(outfile, "GIF")
+    img.save(outfile1, "GIF")
+
+    img = Image.open(filename2)
+    img = img.resize(size, Image.ANTIALIAS)
+    img.save(outfile2, "GIF")
 
 except IOError as e:
     print(" An exception occured '%s'" % e)
